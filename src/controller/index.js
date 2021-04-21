2deeea799aed95cd06101a8a146b66446dcc30ae
Eduardo Amorim <eduardohmamorim@gmail.com>
2021-04-20 21:55:40 -0300

    terminamos toda a parte de pessoas. falta matriculas

--- src/controller/index.js
+++ src/controller/index.js
@@ -17,20 +17,61 @@ const pessoas = await Pessoas.findByPk(id)
 }
 
 
-
-const cadastrar = (req, res) => {
-    Pessoas.create ({
-      nome: req.body.nome,
-      ativo: req.body.ativo,
-      email: req.body.email,
-      role: req.body.role     
+// CADASTRAR UM USUÁRIO
+const cadastrar = async (req, res) => {
+  let { nome, ativo, email, role } = req.query
+  let pessoaCadastrada = await Pessoas.create ({
+    nome: nome,
+    ativo: ativo,
+    email: email,
+    role: role
     })
-  }  
+    res.json(pessoaCadastrada);
+}
+
+//Pensar em uma maneira de atualizar qualquer campo em uma query só
+//Só conseguimos atualizar um "nome" dado um "email"
+const atualizar = async (req, res) => {
+  let { id, nome, email } = req.query
+  let pessoaAtualizada = await Pessoas.update(
+    {
+      nome: nome
+    },
+    {
+      where : {email: email}
+    }
+  );
+  res.json(pessoaAtualizada)
+}
+
+// DELETAR USUÁRIO DADO UM ID
+const deletar = async (req, res) => {
+  let { id } = req.params;
+  let pessoaDeletada = await Pessoas.destroy({
+    where: {
+      id: id
+  }
+})
+
+res.json(pessoaDeletada)
+}
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
-     procuraTodos,
-     procuraUm,
-     cadastrar
+    procuraTodos,
+    procuraUm,
+    cadastrar,
+    atualizar,
+    deletar
 }
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/exibirtodos', controller.procuraTodos)
+router.get('/pessoa/exibirtodos', controller.procuraTodos)
 
-router.get('/exibirum/:id', controller.procuraUm)
+router.get('/pessoa/exibirum/:id', controller.procuraUm)
 
-router.post('/cadastrar', controller.cadastrar)
+router.post('/pessoa/cadastrar', controller.cadastrar)
+
+router.put('/pessoa/atualizar', controller.atualizar)
+
+router.delete('/pessoa/deletar/:id', controller.deletar)
+
+
+// ROTA MATRICULAS
+router.get('/matricula/exibirtodos', controller.procuraTodos)
+
+router.get('/matricula/exibirum/:id', controller.procuraUm)
+
+router.post('/matricula/cadastrar', controller.cadastrar)
+
+router.put('/matricula/atualizar', controller.atualizar)
+
+router.delete('/matricula/deletar/:id', controller.deletar)
 
 module.exports = router;
 
